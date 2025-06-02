b34bf1466ac232c2d4b108c3c92dfce7f38ac7e0
Adarsh-Chaubey03 <adarsh03chaubey@gmail.com>
2025-06-02 11:55:44 +0530

    .

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -1,11 +1,16 @@
+import Card from "./component/Card"
 
 
 function App() {
 
 
   return (
-<h1>Adarsh with vite</h1>
-  )
+    <>
+<h1 className="bg-green-400 text-black rounded-x">Adarsh with vite</h1>
+
+  <Card channel = "alpha"/>
+  </>
+    )
 }
 
 export default App

a7bfa54cab1daadf3fb76a31507a655979a07ee6
Adarsh-Chaubey03 <adarsh03chaubey@gmail.com>
2025-06-14 18:30:32 +0530

    added context api

--- 08_Context-Api-A/src/components/Login.jsx
+++ 08_Context-Api-A/src/components/Login.jsx
@@ -1,14 +1,25 @@
-import React, {useState, useContext} from 'react'
+import React, { useState, useContext } from 'react'
+import UserContext from '../Context/UserContext'
 
 function Login() {
-    const[username,setUsername]=useState('')
-    onst[password,setPassword]=useState('')
-    const handleSubmit = () =>{}
+    const [username, setUsername] = useState('')
+    onst[password, setPassword] = useState('')
+    const { setUser } = useContext(UserContext)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setUser(username, password)
+    }
     return (
         <div>
             <h2>Login</h2>
-        <input type='text' placeholder='username'/>
-        <input type='text' placeholder='password '/>
+            <input type='text'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder='username' />
+                {"...."}
+            <input type='text' placeholder='password ' />
+            <button onClick={handleSubmit}>Submit</button>
         </div>
     )
 }

f14a6c556279160f804c4484459c1e69c214803e
Adarsh-Chaubey03 <adarsh03chaubey@gmail.com>
2025-06-15 11:01:08 +0530

    context api

--- 08_Context-Api-A/src/Context/UserContextProvider.jsx
+++ 08_Context-Api-A/src/Context/UserContextProvider.jsx
@@ -5,7 +5,7 @@ import UserContext from "./UserContext";
 const UserContextProvider = (children) =>{
     const [ user, setUser] = React.useState(null)
 return(
-    <UserContext.Provider>
+    <UserContext.Provider value={{user,setUser}}>
     {children}
     </UserContext.Provider>
 )
